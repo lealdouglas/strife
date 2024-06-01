e25ddd2a1b318e9ba97d1aed28d3c686b8d965c3
Leal <douglas.sleal@outlook.com>
2024-06-01 19:16:13 -0300

    es

--- azsetup/lakehouse/outputs.tf
+++ azsetup/lakehouse/outputs.tf
@@ -4,14 +4,14 @@ output "resource_group_name" {
   value = "[tf-dtmaster-dougsll] Resource Group criado com sucesso ${azurerm_resource_group.this.name}/"
 }
 
-output "storage_name" {
-  value = "[tf-dtmaster-dougsll] Storage Account Gen2 criado com sucesso ${azurerm_storage_account.this.name}/"
-}
+# output "storage_name" {
+#   value = "[tf-dtmaster-dougsll] Storage Account Gen2 criado com sucesso ${azurerm_storage_account.this.name}/"
+# }
 
-output "databricks_host" {
-  value = "[tf-dtmaster-dougsll] Databricks endpoint criado com sucesso: https://${azurerm_databricks_workspace.this.workspace_url}/"
-}
+# output "databricks_host" {
+#   value = "[tf-dtmaster-dougsll] Databricks endpoint criado com sucesso: https://${azurerm_databricks_workspace.this.workspace_url}/"
+# }
 
-output "databricks_id" {
-  value = "[tf-dtmaster-dougsll] Databricks endpoint criado com sucesso: https://${azurerm_databricks_workspace.this.workspace_id}/"
-}
+# output "databricks_id" {
+#   value = "[tf-dtmaster-dougsll] Databricks endpoint criado com sucesso: https://${azurerm_databricks_workspace.this.workspace_id}/"
+# }
