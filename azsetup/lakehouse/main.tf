d4d5601e1457f86d9b5bbac5822259d4cd520ec2
Leal <douglas.sleal@outlook.com>
2024-06-01 19:14:48 -0300

    test

--- azsetup/lakehouse/main.tf
+++ azsetup/lakehouse/main.tf
@@ -31,26 +31,26 @@ resource "azurerm_resource_group" "this" {
   tags     = local.tags
 }
 
-# Create a storage account gen2 in resource group
-resource "azurerm_storage_account" "this" {
-  name                      = "sta${local.suffix_concat}raw"
-  resource_group_name       = azurerm_resource_group.this.name
-  location                  = var.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  account_kind              = "StorageV2"
-  access_tier               = "Hot"
-  is_hns_enabled            = true
-  shared_access_key_enabled = true
-  min_tls_version           = "TLS1_2"
-  tags                      = local.tags
-}
+# # Create a storage account gen2 in resource group
+# resource "azurerm_storage_account" "this" {
+#   name                      = "sta${local.suffix_concat}raw"
+#   resource_group_name       = azurerm_resource_group.this.name
+#   location                  = var.location
+#   account_tier              = "Standard"
+#   account_replication_type  = "LRS"
+#   account_kind              = "StorageV2"
+#   access_tier               = "Hot"
+#   is_hns_enabled            = true
+#   shared_access_key_enabled = true
+#   min_tls_version           = "TLS1_2"
+#   tags                      = local.tags
+# }
 
-resource "azurerm_databricks_workspace" "this" {
-  location                    = azurerm_resource_group.this.location
-  resource_group_name         = azurerm_resource_group.this.name
-  name                        = "adb${local.suffix_concat}"
-  sku                         = "premium"
-  managed_resource_group_name = "rsg${local.suffix_concat}-workspace"
-  tags                        = local.tags
-}
+# resource "azurerm_databricks_workspace" "this" {
+#   location                    = azurerm_resource_group.this.location
+#   resource_group_name         = azurerm_resource_group.this.name
+#   name                        = "adb${local.suffix_concat}"
+#   sku                         = "premium"
+#   managed_resource_group_name = "rsg${local.suffix_concat}-workspace"
+#   tags                        = local.tags
+# }
