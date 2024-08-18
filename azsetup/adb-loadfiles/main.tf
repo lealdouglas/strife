888b317dd24e3ff29dd7f8457cccc81d9972f786
Douglas Leal <douglas.sleal@outlook.com>
2024-08-18 19:42:55 -0300

    Update main.tf

--- azsetup/adb-loadfiles/main.tf
+++ azsetup/adb-loadfiles/main.tf
@@ -1,4 +1,47 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      # version = "=21.90.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "=1.15.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_databricks_workspace" "this" {
+  name                = local.databricks_workspace_name
+  resource_group_name = local.resource_group
+}
+
+locals {
+  databricks_workspace_host = data.azurerm_databricks_workspace.this.workspace_url
+}
+
+// Provider for databricks workspace
+provider "databricks" {
+  host = local.databricks_workspace_host
+}
+
+// Provider for databricks account
+provider "databricks" {
+  alias               = "azure_account"
+  host                = "https://accounts.azuredatabricks.net"
+  account_id          = var.account_id
+  azure_client_id     = var.azure_client_id
+  azure_client_secret = var.azure_client_secret
+  azure_tenant_id     = var.azure_tenant_id
+  auth_type           = "azure-client-secret"
+}
+
+
 variable "job_name" {
   description = "A name for the job."
   type        = string
