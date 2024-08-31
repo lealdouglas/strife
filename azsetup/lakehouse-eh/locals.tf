622d4b321b2bec0379c90c2261eb9eae08318dc0
Leal <douglas.sleal@outlook.com>
2024-08-31 14:37:23 -0300

    remove eh

--- azsetup/lakehouse-eh/locals.tf
+++
@@ -1,8 +0,0 @@
-// use locals for customizing vars
-locals {
-
-  suffix_concat             = var.project_code
-  tags                      = merge(var.tags, { "env" = var.envv })
-  resource_group            = "rsg${local.suffix_concat}"
-  databricks_workspace_name = "adb${local.suffix_concat}"
-}
