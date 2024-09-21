1f6319d98aeecb13223f9c22ff220329a05f6daf
Leal <douglas.sleal@outlook.com>
2024-09-21 19:18:36 -0300

    fix

--- azsetup/lakehouse-adbuc/locals.tf
+++ azsetup/lakehouse-adbuc/locals.tf
@@ -1,7 +1,7 @@
 // use locals for customizing vars
 locals {
-  suffix_concat             = var.domain
-  catalog_name              = var.catalog
+  suffix_concat             = "d${var.domain}"
+  catalog_name              = "c${var.catalog}"
   tags                      = merge({ "domain" = var.domain }, { "project" = var.project }, { "env" = var.envv })
   resource_group            = "rsg${local.suffix_concat}"
   databricks_workspace_name = "adb${local.suffix_concat}"
