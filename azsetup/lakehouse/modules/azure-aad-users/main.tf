1ac1daf4a5efea8d6dfdda012469a8965a88981e
Leal <douglas.sleal@outlook.com>
2024-09-28 14:24:49 -0300

    rs

--- azsetup/lakehouse/modules/azure-aad-users/main.tf
+++ azsetup/lakehouse/modules/azure-aad-users/main.tf
@@ -8,6 +8,12 @@ resource "azuread_group" "data_engineers" {
   security_enabled = true
 }
 
+resource "azuread_user" "fulano" {
+  display_name        = "Fulano"
+  password            = "fulano123"
+  user_principal_name = "fulano@fulano.com"
+}
+
 # # Criando um grupo de Data Engineers
 # resource "azuread_group" "account_unity_admin" {
 #   display_name     = "account_unity_admin"
@@ -15,30 +21,30 @@ resource "azuread_group" "data_engineers" {
 #   security_enabled = true
 # }
 
-resource "azuread_application" "this" {
-  display_name = "spngroup${var.suffix_concat}"
-  owners       = [data.azurerm_client_config.current.object_id]
-}
+# resource "azuread_application" "this" {
+#   display_name = "spngroup${var.suffix_concat}"
+#   owners       = [data.azurerm_client_config.current.object_id]
+# }
 
-resource "azuread_service_principal" "this" {
-  client_id                    = azuread_application.this.client_id
-  app_role_assignment_required = false
-  owners                       = [data.azurerm_client_config.current.object_id]
-  account_enabled              = true
-}
+# resource "azuread_service_principal" "this" {
+#   client_id                    = azuread_application.this.client_id
+#   app_role_assignment_required = false
+#   owners                       = [data.azurerm_client_config.current.object_id]
+#   account_enabled              = true
+# }
 
-resource "time_rotating" "month" {
-  rotation_days = 30
-}
+# resource "time_rotating" "month" {
+#   rotation_days = 30
+# }
 
-resource "azuread_service_principal_password" "this" {
-  service_principal_id = azuread_service_principal.this.object_id
-  rotate_when_changed  = { rotation = time_rotating.month.id }
-}
+# resource "azuread_service_principal_password" "this" {
+#   service_principal_id = azuread_service_principal.this.object_id
+#   rotate_when_changed  = { rotation = time_rotating.month.id }
+# }
 
 resource "azuread_group_member" "user4_member" {
   group_object_id  = azuread_group.data_engineers.id
-  member_object_id = azuread_service_principal.this.object_id
+  member_object_id = azuread_user.fulano.object_id
 }
 
 # resource "azuread_group_member" "user5_member" {
