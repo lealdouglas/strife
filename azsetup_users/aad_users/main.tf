63bf03e0eb3882bfefac734159b190ac7ad35fd4
Leal <douglas.sleal@outlook.com>
2024-09-28 16:14:27 -0300

    tst

--- azsetup_users/aad_users/main.tf
+++ azsetup_users/aad_users/main.tf
@@ -1,43 +1,26 @@
 data "azurerm_client_config" "current" {
 }
 
-# #Criando um grupo de Data Engineers
-# resource "azuread_group" "data_engineers" {
-#   display_name     = "data_engineer"
-#   description      = "Group for Data Engineers"
-#   security_enabled = true
-# }
+resource "azuread_user" "fulano" {
+  display_name        = "Fulano"
+  password            = "SecretP@sswd99!"
+  user_principal_name = "fulano@gabygouveahotmail.onmicrosoft.com"
+}
 
-# resource "azuread_user" "fulano" {
-#   display_name        = "Fulano"
-#   password            = "SecretP@sswd99!"
-#   user_principal_name = "fulano@gabygouveahotmail.onmicrosoft.com"
-# }
-
-# resource "azuread_group_member" "user4_member" {
-#   group_object_id  = azuread_group.data_engineers.id
-#   member_object_id = azuread_user.fulano.object_id
-# }
-
-# resource "azuread_group_member" "user5_member" {
-#   group_object_id  = azuread_group.data_engineers.id
-#   member_object_id = "ea4d5a73-3bb2-4de6-ad62-6dcbf9234d6b"
-# }
-
-
-data "azuread_user" "example" {
+data "azuread_user" "you" {
   user_principal_name = "gaby-gouvea_hotmail.com#EXT#@gabygouveahotmail.onmicrosoft.com"
 }
 
-resource "azuread_group" "example" {
-  display_name     = "MyGroup"
+resource "azuread_group" "dt" {
+  display_name     = "data_engineer"
+  description      = "Group for Data Engineers"
   owners           = [data.azurerm_client_config.current.object_id]
   security_enabled = true
 
   members = [
-    data.azuread_user.example.object_id,
+    data.azuread_user.you.object_id,
     /* more users */
   ]
 
-  depends_on = [data.azuread_user.example]
+  depends_on = [azuread_user.fulano, data.azuread_user.you]
 }
