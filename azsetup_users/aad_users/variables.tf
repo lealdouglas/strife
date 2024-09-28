5b3a309419243a39b68483c172f3664c8061b77b
Leal <douglas.sleal@outlook.com>
2024-09-28 15:03:22 -0300

    vv

---
+++ azsetup_users/aad_users/variables.tf
@@ -0,0 +1,47 @@
+variable "domain" {
+  type    = string
+  default = "domain"
+}
+
+variable "catalog" {
+  type    = string
+  default = "catalog"
+}
+
+variable "project" {
+  type    = string
+  default = "project"
+}
+
+
+variable "account_id" {
+  description = "Azure databricks account id"
+  default     = "00000000-0000-0000-0000-000000000000"
+}
+
+variable "aad_groups" {
+  description = "List of AAD groups that you want to add to Databricks account"
+  type        = list(string)
+  default     = ["data_engineer"]
+}
+
+
+variable "envv" {
+  type    = string
+  default = "dtm"
+}
+
+variable "azure_client_id" {
+  description = "Azure client id"
+  default     = "000000"
+}
+
+variable "azure_client_secret" {
+  description = "Azure client secret id"
+  default     = "000000"
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant id"
+  default     = "000000"
+}
