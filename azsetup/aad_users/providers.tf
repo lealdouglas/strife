d3d72bc7d547e65466649f886e71d566d0502d9f
Leal <douglas.sleal@outlook.com>
2024-09-28 17:53:32 -0300

    add

--- azsetup/aad_users/providers.tf
+++ azsetup/aad_users/providers.tf
@@ -9,25 +9,8 @@ terraform {
   }
 }
 
+# Provedor Azure
+# Azure provider
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
-// Provider for databricks workspace
-provider "databricks" {
-  alias     = "workspace"
-  host      = azurerm_databricks_workspace.this.workspace_url
-  auth_type = "azure-cli"
-}
-
-// Provider for databricks account
-provider "databricks" {
-  alias      = "azure_account"
-  host       = "https://accounts.azuredatabricks.net"
-  account_id = azurerm_databricks_workspace.this.workspace_id
-  auth_type  = "azure-cli"
+  features {}
 }
