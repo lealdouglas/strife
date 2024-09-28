bcd90115cc75f935cff8c54589dc7abd6deb13ff
Leal <douglas.sleal@outlook.com>
2024-09-28 15:52:19 -0300

    tst

--- azsetup_users/aad_users/main.tf
+++ azsetup_users/aad_users/main.tf
@@ -1,25 +1,40 @@
 data "azurerm_client_config" "current" {
 }
 
-#Criando um grupo de Data Engineers
-resource "azuread_group" "data_engineers" {
-  display_name     = "data_engineer"
-  description      = "Group for Data Engineers"
+# #Criando um grupo de Data Engineers
+# resource "azuread_group" "data_engineers" {
+#   display_name     = "data_engineer"
+#   description      = "Group for Data Engineers"
+#   security_enabled = true
+# }
+
+# resource "azuread_user" "fulano" {
+#   display_name        = "Fulano"
+#   password            = "SecretP@sswd99!"
+#   user_principal_name = "fulano@gabygouveahotmail.onmicrosoft.com"
+# }
+
+# resource "azuread_group_member" "user4_member" {
+#   group_object_id  = azuread_group.data_engineers.id
+#   member_object_id = azuread_user.fulano.object_id
+# }
+
+# resource "azuread_group_member" "user5_member" {
+#   group_object_id  = azuread_group.data_engineers.id
+#   member_object_id = "ea4d5a73-3bb2-4de6-ad62-6dcbf9234d6b"
+# }
+
+
+data "azuread_user" "example" {
+  user_principal_name = "gaby-gouvea_hotmail.com#EXT#@gabygouveahotmail.onmicrosoft.com"
+}
+
+resource "azuread_group" "example" {
+  display_name     = "my_group"
   security_enabled = true
 }
 
-resource "azuread_user" "fulano" {
-  display_name        = "Fulano"
-  password            = "SecretP@sswd99!"
-  user_principal_name = "fulano@gabygouveahotmail.onmicrosoft.com"
-}
-
-resource "azuread_group_member" "user4_member" {
-  group_object_id  = azuread_group.data_engineers.id
-  member_object_id = azuread_user.fulano.object_id
-}
-
-resource "azuread_group_member" "user5_member" {
-  group_object_id  = azuread_group.data_engineers.id
-  member_object_id = "ea4d5a73-3bb2-4de6-ad62-6dcbf9234d6b"
+resource "azuread_group_member" "example" {
+  group_object_id  = azuread_group.example.id
+  member_object_id = data.azuread_user.example.id
 }
