c0d8df20157e61b0752b0cc6c3787afc9aa2f210
Leal <douglas.sleal@outlook.com>
2024-09-29 17:02:25 -0300

    add

--- azsetup/aad_users/main.tf
+++ azsetup/aad_users/main.tf
@@ -7,6 +7,23 @@ resource "azuread_user" "this" {
   user_principal_name = "luke@${var.domain_azure}"
 }
 
+resource "azuread_user" "this" {
+  display_name        = "Leia Skywalker"
+  password            = "SecretP@sswd98!"
+  user_principal_name = "leia@${var.domain_azure}"
+}
+
+resource "azuread_user" "this" {
+  display_name        = "Obi Wan"
+  password            = "SecretP@sswd97!"
+  user_principal_name = "obi@${var.domain_azure}"
+}
+
+resource "azuread_user" "this" {
+  display_name        = "Jar Jar binks"
+  password            = "SecretP@sswd96!"
+  user_principal_name = "jarjar@${var.domain_azure}"
+}
 
 # data "azuread_user" "principal_name" {
 #   user_principal_name = var.user_principal_name
--- azsetup/aad_users/outputs.tf
+++ azsetup/aad_users/outputs.tf
@@ -6,7 +6,7 @@ output "user" {
   value = "[tf-dtmaster-dougsll] Novo usuario criado : ${azuread_user.this.display_name}"
 }
 
-output "principal" {
-  value = "[tf-dtmaster-dougsll] Usuario principal vinculado ao grupo criado : ${data.azuread_user.principal_name.display_name}"
-}
+# output "principal" {
+#   value = "[tf-dtmaster-dougsll] Usuario principal vinculado ao grupo criado : ${data.azuread_user.principal_name.display_name}"
+# }
 
